e57898af28cac9a416f20f50e528ecfa8bdd9f6c
Aydin Burak 'Kakule' Kuscu <a.brkkuscu@gmail.com>
2024-08-02 17:58:44 +0300

    Add sidebar and crude layout

--- src/index.js
+++ src/index.js
@@ -2,14 +2,25 @@ import TodoItem from './todoItem';
 import Project from './project';
 import Priority from './priority';
 import { addDays } from 'date-fns';
-import todoItemRender from './todoItemRender';
 import projectRender from './projectRender';
 import './style.css';
 
-const body = document.querySelector('body');
+const content = document.querySelector('.content');
+const projectList = document.querySelector('.sidebar__project-list');
 
-const project = new Project();
+const projects = [];
+projects.push(new Project('My Project'));
 
-project.addTodoItem(new TodoItem(project, 'First Todo', 'Heyo', addDays(new Date(), 3), Priority.LOW));
-project.addTodoItem(new TodoItem(project, 'First Todo', 'Heyo', addDays(new Date(), 3), Priority.LOW));
-body.appendChild(projectRender(project));
+projects[0].addTodoItem(new TodoItem(projects[0], 'First Todo', 'Heyo', addDays(new Date(), 3), Priority.LOW));
+projects[0].addTodoItem(new TodoItem(projects[0], 'First Todo', 'Heyo', addDays(new Date(), 3), Priority.LOW));
+content.appendChild(projectRender(projects[0]));
+projectListRender();
+
+function projectListRender() {
+    projects.forEach(project => {
+    const li = document.createElement('li');
+    li.classList.add('sidebar__project');
+    li.textContent = project.name;
+    projectList.appendChild(li);
+  });
+}
